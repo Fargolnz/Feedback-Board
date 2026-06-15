2c0328b6d9b920abc6bf69f3304fc6b2e5caf36d
Fargolnz <fargol.nz@gmail.com>
2026-06-16 01:02:03 +0330

    Dashboard table style improved

--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from .database import Base
 from .database import engine
-
 from .routes import router
+
+from . import models
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+app.mount(
+    "/static",
+    StaticFiles(directory="app/static"),
+    name="static"
+)
 
 app.include_router(router)
